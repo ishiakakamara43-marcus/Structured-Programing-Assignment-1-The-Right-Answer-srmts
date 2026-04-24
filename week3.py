b7b3c6424d11335c9f7e949ce3e2ce780968577c
ishiakakamara43-marcus <ishiakakamara43@gmail.com>
2026-04-24 12:26:26 +0100

    first commit

---
+++ week3.py
@@ -0,0 +1,9 @@
+FirstName = input("Enter your first name:")
+lastName = input("Enter your last name:")
+age = input("Enter your age:")
+
+print(FirstName)
+print(lastName)
+print(age)
+
+print(FirstName+" "+ lastName)
